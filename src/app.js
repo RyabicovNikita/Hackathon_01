68992b31cf72561a2608f0583d6156867ab8afe9
RyabicovNikita <RyabicovN1k@yandex.ru>
2024-07-13 00:25:50 +0300

    add implementation in app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
+import { onContextMenu } from './actionsForEvent';
+import { ContextMenu } from './menu';
 import './styles.css'
+
+//Создаём экземпляр класса контестного меню
+const contextMenu = new ContextMenu();
+
+//Здесь вызываем метод .add, передавая туда модуль, тем самым добавляя его в меню (не забывая импортировать)
+
+//Listener на вызов контекстного меню при нажатии ПКМ
+document.body.addEventListener('contextmenu', onContextMenu.bind(contextMenu));
+
+
+
+
+
 
