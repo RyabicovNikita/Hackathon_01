af3f4507e728763d8fd4fce63f7265caa9ccbf9d
Nuriev Azat <azattix@gmail.com>
2024-07-13 23:45:18 +0300

    add module to context menu

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
+
 import { onContextMenu } from './actionsForEvent';
 import { ContextMenu } from './menu';
+import BackgroundModule from './modules/background.module';
+import ShapeModule from './modules/shape.module';
 import './styles.css'
+
 
 //Создаём экземпляр класса контестного меню
 const contextMenu = new ContextMenu();
+
+
+//Добавляем модуль в контекстное меню
+const backgroundModule = new BackgroundModule('background', 'Поменять цвет');
+contextMenu.add(backgroundModule);
+
+const shapeModule = new ShapeModule('shape', 'Поменять фигуру');
+contextMenu.add(shapeModule);
 
 //Здесь вызываем метод .add, передавая туда модуль, тем самым добавляя его в меню (не забывая импортировать)
 
 //Listener на вызов контекстного меню при нажатии ПКМ
 document.body.addEventListener('contextmenu', onContextMenu.bind(contextMenu));
-
-
-
 
 
 
