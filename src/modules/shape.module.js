a1107ff9937c69586245fb9f0c2a61a57a1b4125
Nuriev Azat <azattix@gmail.com>
2024-07-13 23:41:28 +0300

    add base fuctional for shapeModule

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,11 +1,44 @@
+// Модуль "Поменять фигуру"
 import { Module } from '../core/module'
 
 export default class ShapeModule extends Module {
     constructor(type, text) {
         super(type, text);
     }
-    //Сюда добавляем реализацию
     trigger() {
-        console.log("ShapeModule")
+        let shape;
+        function generateRandomShape() {
+            if (shape) {
+                return;
+            }
+            function getRandomNumber(min, max) {
+                return Math.floor(Math.random() * (max - min + 1) + min);
+            }
+            shape = document.createElement('div');
+
+            const color = `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)})`;
+
+            shape.style.backgroundColor = color;
+            shape.style.width = `${getRandomNumber(50, 200)}px`;
+            shape.style.height = `${getRandomNumber(50, 200)}px`;
+
+            shape.style.position = 'absolute';
+            shape.style.top = `${getRandomNumber(0, window.innerHeight - 200)}px`;
+            shape.style.left = `${getRandomNumber(0, window.innerWidth - 200)}px`;
+
+            document.body.append(shape);
+
+            if (getRandomNumber(0, 1)) {
+                shape.style.borderRadius = '50%';
+            } else {
+                shape.style.borderRadius = '0';
+            }
+            setTimeout(() => {
+                shape.style.opacity = 0;
+                shape.style.transition = 'opacity 10s';
+            }, 0);
+        }
+        document.addEventListener(`click`, generateRandomShape);
     }
+
 }
