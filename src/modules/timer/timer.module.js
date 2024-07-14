b93358d09a37c20fae8407cc7a6d0add36494d55
Vitalya <vitalikt_22@mail.ru>
2024-07-14 22:27:00 +0400

    adding feature of removing previous timer

--- src/modules/timer/timer.module.js
+++ src/modules/timer/timer.module.js
@@ -5,12 +5,28 @@ import '../timer/timer.css'
 export default class TimerModule extends Module {
   constructor(type, text) {
     super(type, text)
+    // Переменная для хранения идентификатора интервала
+    this.interval = null
   }
+
   trigger() {
+    // Останавливаем текущий таймер, если он существует
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+
+      const existingTimerElement = document.querySelector('.timer-element')
+      // Удаляем существующий элемент таймера
+      if (existingTimerElement) {
+        existingTimerElement.remove()
+      }
+    }
+
     const time = prompt('Насколько секунд желаете засечь таймер?').trim()
     let seconds = parseInt(time)
-
+    //Проверяем на тип
     if (!isNaN(seconds) && seconds > 0) {
+      //создаём элемент в DOM
       const timerElement = document.createElement('div')
       document.body.appendChild(timerElement)
       const resultOfRandomColor = getRandomColor()
@@ -19,14 +35,16 @@ export default class TimerModule extends Module {
       function removeTimerElement() {
         timerElement.remove()
       }
-      const interval = setInterval(() => {
+      //таймер
+      this.interval = setInterval(() => {
         timerElement.className = 'timer-element'
         if (seconds > 0) {
           timerElement.textContent = `Осталось секунд: ${seconds}`
           seconds--
         } else {
           timerElement.textContent = 'Время вышло!'
-          clearInterval(interval)
+          clearInterval(this.interval)
+          this.interval = null
           setTimeout(removeTimerElement, 2000)
         }
       }, 1000)
