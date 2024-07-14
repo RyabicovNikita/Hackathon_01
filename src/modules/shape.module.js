0eaf2bb15102a46b6d4cda2d24f9fb14a5f749f8
Nuriev Azat <azattix@gmail.com>
2024-07-14 14:02:26 +0300

    shape module  was optimized instead of two random number functions, one common function was used

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,6 @@
 // Модуль "Поменять фигуру"
 import { Module } from '../core/module'
+import { random } from '../utils'
 
 export default class ShapeModule extends Module {
     constructor(type, text) {
@@ -11,24 +12,21 @@ export default class ShapeModule extends Module {
             if (shape) {
                 return;
             }
-            function getRandomNumber(min, max) {
-                return Math.floor(Math.random() * (max - min + 1) + min);
-            }
             shape = document.createElement('div');
 
-            const color = `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)})`;
+            const color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 
             shape.style.backgroundColor = color;
-            shape.style.width = `${getRandomNumber(50, 200)}px`;
-            shape.style.height = `${getRandomNumber(50, 200)}px`;
+            shape.style.width = `${random(50, 200)}px`;
+            shape.style.height = `${random(50, 200)}px`;
 
             shape.style.position = 'absolute';
-            shape.style.top = `${getRandomNumber(0, window.innerHeight - 200)}px`;
-            shape.style.left = `${getRandomNumber(0, window.innerWidth - 200)}px`;
+            shape.style.top = `${random(0, window.innerHeight - 200)}px`;
+            shape.style.left = `${random(0, window.innerWidth - 200)}px`;
 
             document.body.append(shape);
 
-            if (getRandomNumber(0, 1)) {
+            if (random(0, 1)) {
                 shape.style.borderRadius = '50%';
             } else {
                 shape.style.borderRadius = '0';
@@ -40,5 +38,4 @@ export default class ShapeModule extends Module {
         }
         document.addEventListener(`click`, generateRandomShape);
     }
-
 }
