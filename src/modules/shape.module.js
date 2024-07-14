732faa61707e99e72a868e60e99f7cbe7a616f0d
Nuriev Azat <azattix@gmail.com>
2024-07-14 14:20:59 +0300

    in shape module function geRandomShape has been removed

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -8,34 +8,31 @@ export default class ShapeModule extends Module {
     }
     trigger() {
         let shape;
-        function generateRandomShape() {
-            if (shape) {
-                return;
-            }
-            shape = document.createElement('div');
-
-            const color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
-
-            shape.style.backgroundColor = color;
-            shape.style.width = `${random(50, 200)}px`;
-            shape.style.height = `${random(50, 200)}px`;
-
-            shape.style.position = 'absolute';
-            shape.style.top = `${random(0, window.innerHeight - 200)}px`;
-            shape.style.left = `${random(0, window.innerWidth - 200)}px`;
-
-            document.body.append(shape);
-
-            if (random(0, 1)) {
-                shape.style.borderRadius = '50%';
-            } else {
-                shape.style.borderRadius = '0';
-            }
-            setTimeout(() => {
-                shape.style.opacity = 0;
-                shape.style.transition = 'opacity 10s';
-            }, 0);
+        if (shape) {
+            return;
         }
-        generateRandomShape();
+        shape = document.createElement('div');
+
+        const color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+
+        shape.style.backgroundColor = color;
+        shape.style.width = `${random(50, 200)}px`;
+        shape.style.height = `${random(50, 200)}px`;
+
+        shape.style.position = 'absolute';
+        shape.style.top = `${random(0, window.innerHeight - 200)}px`;
+        shape.style.left = `${random(0, window.innerWidth - 200)}px`;
+
+        document.body.append(shape);
+
+        if (random(0, 1)) {
+            shape.style.borderRadius = '50%';
+        } else {
+            shape.style.borderRadius = '0';
+        }
+        setTimeout(() => {
+            shape.style.opacity = 0;
+            shape.style.transition = 'opacity 10s';
+        }, 0);
     }
 }
